d262b44e75c4f18f21ae8a4fa0e2a4239fc94518
jamalamar <jamal_ab22@hotmail.com>
2019-02-10 10:34:34 -0600

    created a ref property on the text field

--- src/Account/index.js
+++ src/Account/index.js
@@ -18,7 +18,7 @@ class Account extends Component {
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
+        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
         <input type="button" value="Deposit" />
         <input type="button" value="Withdraw" />
       </div>
