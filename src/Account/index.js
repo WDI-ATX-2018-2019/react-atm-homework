abc76a5dd03dee5e97c175b115d70f12178e53cf
jamalamar <jamal_ab22@hotmail.com>
2019-02-10 10:55:05 -0600

    working button function

--- src/Account/index.js
+++ src/Account/index.js
@@ -13,13 +13,29 @@ class Account extends Component {
     balance: 0
   }
 
+  handleDepositClick = (event) => {
+    // It is good practice to still prevent default behavior
+    event.preventDefault();
+    // set a local variable to the amount entered in the text box.
+    const amount = parseInt(this.inputBox.value);
+    // set a local variable to the new balance based off of the original balance + amount
+    const newBalance = this.state.balance + amount;
+    // set the balance to the newBalance using the setState method (necessary)
+    this.setState({
+      balance: newBalance
+    })
+    // empty out the text box in this component
+    this.inputBox.value = ''
+  }
+  
+
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">$0</div>
+        <div className="balance">{this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
-        <input type="button" value="Deposit" />
+        <input type="button" value="Deposit" onClick={this.handleDepositClick}/>
         <input type="button" value="Withdraw" />
       </div>
     )
