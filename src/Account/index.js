1c8b492811125eb3200abb86cd6f14dc4e3a54e4
jwerbin95 <jwerbin95@gmail.com>
2019-02-11 00:23:51 -0600

    started working on react app

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      balance: 0
+    }
+  }
+  handleDepositClick(event){
+    event.preventDefault()
+    const inputBox = document.getElementById("amount")
+    const inputValue = parseInt(this.inputBox.value)
+
+    this.setState({
+      balance: this.state.balance+inputValue
+    })
+  }
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
+        <h2>{this.props.name}</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
+        <input type="text" placeholder="enter an amount" id="amount" ref={(input)=>this.inputBox=input}/>
+        <input type="button" value="Deposit" onClick={this.handleDepositClick}/>
         <input type="button" value="Withdraw" />
       </div>
     )
