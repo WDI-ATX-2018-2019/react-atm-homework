78e53a06b8b49efc309e187867a842f5529c8540
Andrew Aluko <aluko17@icloud.com>
2019-02-11 10:32:28 -0600

    account

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(props){
+          super(props)
+          this.state = {
+            balance: 0
+            }
+          }
+        
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
+        <h2>{this.props.name}</h2>
         <div className="balance">$0</div>
         <input type="text" placeholder="enter an amount" />
         <input type="button" value="Deposit" />
